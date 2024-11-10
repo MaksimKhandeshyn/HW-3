f6288953def1122490e819c44ec97c7343b22fe5
MaksimKhandeshyn <145330399+MaksimKhandeshyn@users.noreply.github.com>
2024-11-10 10:57:27 +0200

    make hw

---
+++ index.js
@@ -0,0 +1,19 @@
+//First
+const result = 5 + 5 + "5";
+console.log(
+  `Result of this adding is: ${result} and his type is ` + typeof result
+);
+//Second
+const myEmail = "maksimhandeshin@gmail.com";
+console.log("Is this email has symbol @? " + myEmail.includes("@"));
+console.log(myEmail.length);
+//Third
+const start = "My";
+const middle = "name";
+const end = "is";
+const fullName = `${start} ${middle} ${end} Viktor`;
+console.log(fullName);
+//Fourth
+let userName = "Oleg";
+let payment = "400₴";
+alert(`Thank you, ${userName}! For payment ${payment}`);
